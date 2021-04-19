e7a21ae2777410072441c746491a09398a0d6129
zachmw2013 <zachmw2013@hotmail.com>
2021-04-19 11:00:35 -0500

    first commit

---
+++ pages/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+from .views import HomePageView, AboutPageView, testPageView, loginpageview, RegisterPageView
+
+
+urlpatterns = [
+   
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('test/', testPageView.as_view(), name='test'),
+    path('', HomePageView.as_view(), name='home'),
+    path('login/', loginpageview.as_view(), name='login'),
+    path('register/', RegisterPageView.as_view(), name='registerpage'),
+    path('register', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path(r'^', views.button),
+    path(r'^output$', views.output, name="script"),
+    #path('test/', views.input, name="input"),
+    #path('test/', views.input),
+    #path('', views.index, name='index'),
+    
+    
+    
+    
+]
---
+++ pages/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+import requests
+import time
+from twelvedata import TDClient
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm
+
+
+
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class testPageView(TemplateView):
+    template_name = 'test.html'
+
+class loginpageview(TemplateView):
+    template_name = 'login.html'
+
+class RegisterPageView(TemplateView):
+    template_name = 'register.html'
+
+
+def index(request):
+    return render(request, 'pages/home.html')
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        #return redirect('accounts/login')
+    else:          
+        form = RegisterForm()
+
+    return render(response, "register.html", {"form":form})
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+
+
+def button(request):
+
+    return render(request, 'test.html')
+
+#def input(request):
+   #if request.method == 'POST':
+    #   userinput = request.POST.get('ticker')
+   #print(userinput)
+   #return render(request,'test.html')
+
+def output(request):
+    userinput = request.POST.get('ticker')
+    td = TDClient(apikey="5ab205ca85414927b3b757076d329696")
+    ts=td.time_series(symbol=userinput, interval="1min", outputsize=1).as_json()
+    print(ts)
+
+    return render(request,'test.html',{'ts':ts})
+    
+
